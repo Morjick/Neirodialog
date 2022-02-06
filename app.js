6a6d1403e62734a5731b1ec58465532f89e6c466
Morjick <matvey-khramov@inbox.ru>
2022-02-06 21:45:55 +0300

    delete

--- app.js
+++ app.js
@@ -12,36 +12,10 @@ app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  
-  res.header("Access-Control-Allow-Methods", "HEAD, OPTIONS, GET, POST, PUT, DELETE")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 
   next()
 })
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain)
-
-app.all('*', function(req, res, next) {
-  var origin = req.get('origin'); 
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-})
 
 app.use('/api/getProducts', require('./routes/getProducts.rout'))
 app.use('/api/basket', require('./routes/basket.route'))
