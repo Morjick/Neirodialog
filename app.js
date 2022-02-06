d823ac10e9449e0238a1e40327528cdeeb126308
Morjick <matvey-khramov@inbox.ru>
2022-02-06 21:37:57 +0300

    second update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'))
 app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "/*")
+  res.header("Access-Control-Allow-Origin", "/")
 
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Origin", "https://neirodialog.ru")
@@ -20,6 +20,14 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 
   next()
+})
+
+app.configure(function () {
+  app.use(allowCrossDomain)
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(express.static(path.join(application_root, "public")))
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
 })
 
 app.all('*', function(req, res, next) {
